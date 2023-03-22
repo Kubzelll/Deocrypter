1fe99e81b82d0967e7dbe0a37578042528a52362
Kubzel <kzelazko32@gmail.com>
2023-03-22 15:52:44 +0100

    Updating to v3.0

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import tkinter as tk
 from tkinter import * 
 from tkinter import PhotoImage, filedialog
 from pystyle import Colors, Colorate
+import os
+from time import sleep
 from tkinter.messagebox import askyesno
+
+
+
 
 '''
 $$\   $$\          $$\                           $$\ 
@@ -33,19 +38,13 @@ $$ | \$$\\$$$$$$  |$$$$$$$  |$$$$$$$$\ \$$$$$$$\ $$ |
 \__|  \__|\______/ \_______/ \________| \_______|\__|       
 """,1))
 
-print(Colorate.DiagonalBackwards(Colors.cyan_to_blue,"Project made by Kubzel",1))
-f = Fernet
-option = int(input("Choose option 1 to overwrite dec/enc file or select 2 to create a new file that will be enc/dec \n"))
+print(Colorate.DiagonalBackwards(Colors.cyan_to_green,"Project made by Kubzel",1))
 
-newfile = False
-if option == 2:
-    newfile = True
-try:
-#getting token to decrypt&encrypt
-    with open("token.key", 'rb') as file:
-        key = file.read()
-except FileNotFoundError:
-    print("No file found, create one!")
+
+
+
+
+f = Fernet
 
 
 def get_file_extension(file_name):
@@ -55,6 +54,7 @@ def get_file_extension(file_name):
     else:
         return None
 
+
 def change_ext(file):
     file = file[:-4]
     ext = get_file_extension(file)
@@ -62,19 +62,38 @@ def change_ext(file):
     file = file + ".dec" + ext
     return file
 
+
+
+
+try:
+#getting token to decrypt&encrypt
+    with open("token.key", 'rb') as file:
+        key = file.read()
+except FileNotFoundError:
+    print("No file found, create one!")
+
+option = int(input("Choose option 1 to overwrite dec/enc file or select 2 to create a new file that will be enc/dec \n"))
+
+newfile = False
+if option == 2:
+    newfile = True
+
+
+
+
+
 def create():
     try:
         token = f.generate_key()
         #open text file
-        with open("token.key", "wb") as openhandler:
-            openhandler.write(token)
-            openhandler.close()
+        with open("token.key", "wb") as file:
+            file.write(token)
+            file.close()
 
         print(token)
-        key = token
         print("Writed")
-    except Exception as e:
-        print(f"ERROR {e}")
+    except:
+        print("ERROR")
 
 
 
@@ -83,9 +102,13 @@ def select_file():
     fileselect = filedialog.askopenfilename(title='Select File',)
     global files 
     files = fileselect
+    info_list.insert(END, f"File selected")
+    info_list.insert(END, f"\n {files}")
 
 
     print(files)
+
+
 
 
 def encrpyt():
@@ -114,12 +137,9 @@ def encrpyt():
     except Exception as e:
         print(files)
         print(f"First select file to encrypt {e}" )
-        
+        info_list.insert(END, f"[!]File not selected or not logged in [!] {e}" )
 
-
-
-
-
+      
 
 def decrypt():
     if newfile == False:
@@ -131,11 +151,13 @@ def decrypt():
             with open(files, "wb") as writedecfile:
                 writedecfile.write(contents_decrypt)
             print("[!]SUCCESSFULLY DECRYPTED[!]")
+            info_list.insert(END, "[!]SUCCESSFULLY DECRYPTED[!]")
 
         
         except Exception as e:
             print(files)
             print(f"First select file to decrypt {e}")
+            info_list.insert(END, f"No file selected or file is already decrypted {e}")
     else:
         try:
             newfilename = change_ext(files)
@@ -147,10 +169,11 @@ def decrypt():
             with open(newfilename, "wb") as writedecfile:
                 writedecfile.write(contents_decrypt)
             print("[!]SUCCESSFULLY DECRYPTED[!]")
-
+            info_list.insert(END, "[!]SUCCESSFULLY DECRYPTED[!]")
         except Exception as e:
             print(files)
             print(f"First select file to decrypt {e}")
+            info_list.insert(END, f"No file selected or file is already decrypted {e}")
 
 
 
@@ -158,6 +181,7 @@ def new_key():
     popup = askyesno("Are you sure?", "This will overwrite your enc/dec token and you will not be able to decrypt all encrypted files with this token. Click yes to continue")
     if popup == True:
         print("Generating new token...")
+        info_list.insert(END, "Generating new token")
         create()
     
 
@@ -168,45 +192,30 @@ def new_key():
 
 root = tk.Tk()
 root.title('Deocrypter')
+info_frame = Frame(root)
+scroolbar = Scrollbar(info_frame)
+info_list = Listbox(info_frame, height=30, width=50, yscrollcommand=scroolbar.set)
+scroolbar.pack(side=RIGHT, fill=Y)
+info_list.pack(side=LEFT, fill=BOTH)
+info_list.pack()
+info_frame.pack()
 
 
-canvas = tk.Canvas(root, height=700, width=700, bg = '#263D42')
-canvas.pack()
-
-frame = tk.Frame(root, bg='white')
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-
-openfile = tk.Button(root, text='Open files', padx=10, 
-                        pady=5, fg='white', bg='#263D42', command=select_file)
+openfile = tk.Button(root, text='Open files', padx=4, 
+                        pady=4, fg='white', bg='#263D42', command=select_file)
 openfile.pack()
 
 
-encrpyt = tk.Button(root, text='Encrpyt files', padx=10,
-                         pady=5, fg='white', bg='#263D42', command=encrpyt)
+encrpyt = tk.Button(root, text='Encrpyt files', padx=4,
+                         pady=4, fg='white', bg='#263D42', command=encrpyt)
 encrpyt.pack()
 
-decrypt = tk.Button(root, text='Decrypt Files', padx=10, pady=5, fg='white', bg='#263D42', command=decrypt)
+decrypt = tk.Button(root, text='Decrypt Files', padx=4, pady=4, fg='white', bg='#263D42', command=decrypt)
 
 decrypt.pack()
 
-createkey = tk.Button(root, text='Create new token', padx=10, pady=5, fg='white', bg='#263D42', command=new_key)
+createkey = tk.Button(root, text='Create new token', padx=4, pady=4, fg='white', bg='#263D42', command=new_key)
 createkey.pack()
 
 
-
 root.mainloop()
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
